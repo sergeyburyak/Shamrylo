fdc55b94af8c618b630ed0f168e2154dbbac57f0
sergeyburyak <60316056+sergeyburyak@users.noreply.github.com>
2020-01-27 17:02:41 +0200

    Add files via upload
    Заготовка для прорисовки фронтэнда

---
+++ Shamrylo/Shamrylo/MainForm.Designer.vb
@@ -0,0 +1,49 @@
+﻿'
+' Created by SharpDevelop.
+' User: buryaksergey
+' Date: 27.01.2020
+' Time: 11:39
+' 
+' To change this template use Tools | Options | Coding | Edit Standard Headers.
+'
+Partial Class MainForm
+	Inherits System.Windows.Forms.Form
+	
+	''' <summary>
+	''' Designer variable used to keep track of non-visual components.
+	''' </summary>
+	Private components As System.ComponentModel.IContainer
+	
+	''' <summary>
+	''' Disposes resources used by the form.
+	''' </summary>
+	''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+	Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+		If disposing Then
+			If components IsNot Nothing Then
+				components.Dispose()
+			End If
+		End If
+		MyBase.Dispose(disposing)
+	End Sub
+	
+	''' <summary>
+	''' This method is required for Windows Forms designer support.
+	''' Do not change the method contents inside the source code editor. The Forms designer might
+	''' not be able to load this method if it was changed manually.
+	''' </summary>
+	Private Sub InitializeComponent()
+		Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(MainForm))
+		Me.SuspendLayout
+		'
+		'MainForm
+		'
+		Me.AutoScaleDimensions = New System.Drawing.SizeF(6!, 13!)
+		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+		Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"),System.Drawing.Image)
+		Me.ClientSize = New System.Drawing.Size(1711, 1228)
+		Me.Name = "MainForm"
+		Me.Text = "Shamrylo"
+		Me.ResumeLayout(false)
+	End Sub
+End Class
---
+++ Shamrylo/Shamrylo/MainForm.vb
@@ -0,0 +1,18 @@
+﻿'
+' Created by SharpDevelop.
+' User: buryaksergey
+' Date: 27.01.2020
+' Time: 11:39
+' 
+' To change this template use Tools | Options | Coding | Edit Standard Headers.
+'
+Public Partial Class MainForm
+	Public Sub New()
+		' The Me.InitializeComponent call is required for Windows Forms designer support.
+		Me.InitializeComponent()
+		
+		'
+		' TODO : Add constructor code after InitializeComponents
+		'
+	End Sub
+End Class
